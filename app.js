18e50821af978e42ef87d354ef3774b946034440
Chris Davis <chris@cstcac.com>
2022-04-20 09:03:37 +0100

    code style

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const db = sqlite.inmem;
 let playercars = [{
   player1carid: 1,
   player2carid: 1,
-  randcarid: 99,
   player1swapcardsremain: 1,
   player2swapcardsremain: 1,
   player1swapsremain: 10,
   player2swapsremain: 10,
   turnsleft: 20,
+  randcarid: 99,
 }];
 
 playercars[0].randcarid = util.getRand();
@@ -41,7 +41,7 @@ app.listen(PORT, () => {
 //-------------------- Routing ----------------------
 // GET / -- this is the home page button
 app.get("/", (req, res) => {
-  const sql = "select 'a' as 'rec_id', c.* from cars as c where car_id = " + playercars[0].player1carid + " union select 'b' as 'rec_id', d.* from cars as d where car_id = " + playercars[0].player2carid + " union select 'c' as 'rec_id' , e.* from cars as e where car_id = " + playercars[0].randcarid
+  const sql = "select 'a' as 'rec_id', c.* from cars as c where car_id = " + playercars[0].player1carid + " union select 'b' as 'rec_id', d.* from cars as d where car_id = " + playercars[0].player2carid + " union select 'c' as 'rec_id' , e.* from cars as e where car_id = " + playercars[0].randcarid;
   db.all(sql, (err, rows) => {
     res.render("index", {
       p1cars: rows[0],
@@ -54,11 +54,11 @@ app.get("/", (req, res) => {
 
 
 app.get("/selectCarPlayer1", (req, res) => {
-  playercars[0].player1carid = playercars[0].randcarid
-  playercars[0].player1swapsremain = playercars[0].player1swapsremain - 1
-  const sql = "select * from cars where car_id = " + playercars[0].player1carid
+  playercars[0].player1carid = playercars[0].randcarid;
+  playercars[0].player1swapsremain = playercars[0].player1swapsremain - 1;
+  const sql = "select * from cars where car_id = " + playercars[0].player1carid;
   db.get(sql, (err, rows) => {
-    res.set({'HX-Trigger': 'nextturn'});
+    res.set({"HX-Trigger": "nextturn"});
     res.render("./includes/p1car", {
       p1cars: rows,
       players: playercars[0]
@@ -67,11 +67,11 @@ app.get("/selectCarPlayer1", (req, res) => {
 });
 
 app.get("/selectCarPlayer2", (req, res) => {
-  playercars[0].player2carid = playercars[0].randcarid
-  playercars[0].player2swapsremain = playercars[0].player2swapsremain - 1
-  const sql = "select * from cars where car_id = " + playercars[0].player2carid
+  playercars[0].player2carid = playercars[0].randcarid;
+  playercars[0].player2swapsremain = playercars[0].player2swapsremain - 1;
+  const sql = "select * from cars where car_id = " + playercars[0].player2carid;
   db.get(sql, (err, rows) => {
-    res.set({'HX-Trigger': 'nextturn'});
+    res.set({"HX-Trigger": "nextturn"});
     res.render("./includes/p2car", {
       p2cars: rows,
       players: playercars[0]
@@ -80,10 +80,10 @@ app.get("/selectCarPlayer2", (req, res) => {
 });
 
 app.get("/nextturn", (req, res) => {
-  playercars[0].turnsleft = playercars[0].turnsleft - 1
-  playercars[0].randcarid = util.getRand()
-  const getNewRandCar = "Select * from cars where car_id=?"
-  res.set({'HX-Trigger': 'refresh'});
+  playercars[0].turnsleft = playercars[0].turnsleft - 1;
+  playercars[0].randcarid = util.getRand();
+  const getNewRandCar = "Select * from cars where car_id=?";
+  res.set({"HX-Trigger": "refresh"});
   db.get(getNewRandCar, playercars[0].randcarid, (err, row) => {
     res.render("./includes/newcar.ejs", {
       randcars: row,
@@ -99,22 +99,22 @@ app.get("/menu", (req, res) => {
 });
 
 app.get("/restart", (req, res) => {
-  playercars[0].player1carid = 1
-  playercars[0].player2carid = 1
-  playercars[0].randcarid = 99
-  playercars[0].player1swapcardsremain = 1
-  playercars[0].player2swapcardsremain = 1
-  playercars[0].player1swapsremain = 10
-  playercars[0].player2swapsremain = 10
-  playercars[0].turnsleft = 20
+  playercars[0].player1carid = 1;
+  playercars[0].player2carid = 1;
+  playercars[0].randcarid = 99;
+  playercars[0].player1swapcardsremain = 1;
+  playercars[0].player2swapcardsremain = 1;
+  playercars[0].player1swapsremain = 10;
+  playercars[0].player2swapsremain = 10;
+  playercars[0].turnsleft = 20;
   res.redirect("/");
 });
 
 
 app.get("/swapPlayer1", (req, res) => {
-  playercars[0].player2carid = playercars[0].randcarid
-  playercars[0].player1swapcardsremain = playercars[0].player1swapcardsremain - 1
-  const sql = "select * from cars where car_id = " + playercars[0].player2carid
+  playercars[0].player2carid = playercars[0].randcarid;
+  playercars[0].player1swapcardsremain = playercars[0].player1swapcardsremain - 1;
+  const sql = "select * from cars where car_id = " + playercars[0].player2carid;
   db.get(sql, (err, rows) => {
     res.set({'HX-Trigger': 'nextturn'});
     res.render("./includes/p2car", {
@@ -126,11 +126,11 @@ app.get("/swapPlayer1", (req, res) => {
 
 
 app.get("/swapPlayer2", (req, res) => {
-  playercars[0].player1carid = playercars[0].randcarid
-  playercars[0].player2swapcardsremain = playercars[0].player2swapcardsremain - 1
-  const sql = "select * from cars where car_id = " + playercars[0].player1carid
+  playercars[0].player1carid = playercars[0].randcarid;
+  playercars[0].player2swapcardsremain = playercars[0].player2swapcardsremain - 1;
+  const sql = "select * from cars where car_id = " + playercars[0].player1carid;
   db.get(sql, (err, rows) => {
-    res.set({'HX-Trigger': 'nextturn'});
+    res.set({"HX-Trigger": "nextturn"});
     res.render("./includes/p1car", {
       p1cars: rows,
       players: playercars[0]
